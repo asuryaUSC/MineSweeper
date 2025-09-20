89f280a20fd9817a3ab090eb62abe4393fc8e145
asuryaUSC <asurya@usc.edu>
2025-09-19 19:04:22 -0700

    Created xml screens and updated gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,9 @@ android {
 dependencies {
 
     implementation(libs.appcompat)
+    implementation("androidx.gridlayout:gridlayout:1.0.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.material:material:1.12.0")
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
